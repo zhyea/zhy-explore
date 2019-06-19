16827f566e4c0c85d70667f8a133f7bc538d61b8
rui.zhang <rui.zhang@tendcloud.com>
2019-06-19 19:35:16 +0800

    alter
    Change-Id: Ic7753fa9eea1a6d65297deaf3a56fbe05bf6d645

--- metrics-explore/src/main/java/org/chobit/metrics/GaugeShow.java
+++ metrics-explore/src/main/java/org/chobit/metrics/GaugeShow.java
@@ -10,25 +10,18 @@ import java.util.concurrent.TimeUnit;
 
 public class GaugeShow {
 
-
     public static void main(String[] args) throws InterruptedException {
         final MetricRegistry metrics = new MetricRegistry();
         ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics).build();
         reporter.start(3, TimeUnit.SECONDS);
 
-        Gauge<String> gauge = new Gauge<String>() {
-            @Override
-            public String getValue() {
-                return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
-            }
-        };
+        Gauge<String> gauge = () -> new SimpleDateFormat("MM-dd HH:mm:ss").format(new Date());
 
         metrics.register("时间计时", gauge);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 3; i++) {
             TimeUnit.SECONDS.sleep(3);
         }
     }
-
 
 }
