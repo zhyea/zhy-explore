f966ded35e83a74a5989775808aa3246128bd3b1
rui.zhang <rui.zhang@tendcloud.com>
2019-07-04 13:18:15 +0800

    add quartz explore
    Change-Id: I0a9b6992b377683aba99ad0bac7e644b6c98acd7

--- quartz-explore/src/main/java/org/chobit/quartz/JobClient.java
+++ quartz-explore/src/main/java/org/chobit/quartz/JobClient.java
@@ -8,14 +8,14 @@ public class JobClient {
 
     public static void main(String[] args) throws SchedulerException {
         MyConfig config = new MyConfig(1);
-        JobProxy proxy = new JobProxy(config);
+        JobRegistry registry = new JobRegistry(config);
 
         Scheduler scheduler = new StdSchedulerFactory().getScheduler();
-        scheduler.setJobFactory(new MyJobFactory(proxy));
+        scheduler.setJobFactory(new MyJobFactory(registry));
 
         scheduler.start();
 
-        for(AbstractJob job : proxy.jobs()){
+        for(AbstractJob job : registry.jobs()){
             scheduler.scheduleJob(job.job(), job.trigger());
         }
     }
