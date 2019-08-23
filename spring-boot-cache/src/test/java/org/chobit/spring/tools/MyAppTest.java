ad8e7eb85090e157775f5f999630e31be25d98ba
rui.zhang <rui.zhang@tendcloud.com>
2019-08-23 23:33:03 +0800

    Caffeine Cache Config
    Change-Id: Ib723772ea02f9b6b6414510f7da82ad585bc5f2a

---
+++ spring-boot-cache/src/test/java/org/chobit/spring/tools/MyAppTest.java
@@ -0,0 +1,26 @@
+package org.chobit.spring.tools;
+
+
+import org.chobit.spring.service.WorkerService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MyAppTest {
+
+    @Autowired
+    private WorkerService service;
+
+    @Test
+    public void get() {
+        service.get(2);
+        service.get(2);
+        service.get(2);
+        service.get(2);
+    }
+
+}
