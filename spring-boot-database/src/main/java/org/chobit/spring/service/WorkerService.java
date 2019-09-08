aba7283d0300471f790cdf60213283d5a0849d43
rui.zhang <rui.zhang@tendcloud.com>
2019-09-09 06:48:50 +0800

    springboot自定义数据库配置
    Change-Id: Ia33dad1cb8b28bec4eff8c6dd46e4b9a2946e8b7

--- spring-boot-database/src/main/java/org/chobit/spring/service/WorkerService.java
+++ spring-boot-database/src/main/java/org/chobit/spring/service/WorkerService.java
@@ -2,7 +2,7 @@ package org.chobit.spring.service;
 
 
 import org.chobit.spring.entity.Worker;
-import org.chobit.spring.service.mapper.WorkerMapper;
+import org.chobit.spring.service.mapper.worker.WorkerMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
--- spring-boot-database/src/test/java/org/chobit/spring/tools/MyAppTest.java
+++ spring-boot-database/src/test/java/org/chobit/spring/tools/MyAppTest.java
@@ -1,7 +1,10 @@
 package org.chobit.spring.tools;
 
 
+import org.chobit.spring.entity.Master;
+import org.chobit.spring.service.MasterService;
 import org.chobit.spring.service.WorkerService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +16,38 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class MyAppTest {
 
     @Autowired
-    private WorkerService service;
+    private WorkerService workerService;
+    @Autowired
+    private MasterService masterService;
+
 
     @Test
-    public void get() {
-        service.create();
-        service.get(1);
+    public void getWorker() {
+        workerService.create();
+        workerService.get(1);
+    }
+
+
+    @Test
+    public void getMaster() {
+        masterService.create();
+        masterService.insert();
+        Master m = masterService.get(1);
+        System.out.println(m);
+    }
+
+
+    @Test
+    public void getMaster2() {
+        masterService.create();
+        try {
+            masterService.get0();
+        } catch (Exception e) {
+            System.out.println("------ occurred error: " + e.getMessage());
+        }
+        masterService.insert();
+        Assert.assertNull(masterService.get(1));
+        Assert.assertNotNull(masterService.get(2));
     }
 
 }
