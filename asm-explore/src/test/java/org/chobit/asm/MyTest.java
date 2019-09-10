baa5b7bc4feea474acf998f43b9cadf7fe91c30a
rui.zhang <rui.zhang@tendcloud.com>
2019-09-10 17:02:16 +0800

    一份可用样本
    Change-Id: I0ba5c887e7759b168b1e4af9382bc07a36bf8e7c

--- asm-explore/src/test/java/org/chobit/asm/MyTest.java
+++ asm-explore/src/test/java/org/chobit/asm/MyTest.java
@@ -3,7 +3,9 @@ package org.chobit.asm;
 public class MyTest {
 
     public static void main(String[] args) {
-        System.out.println(hello());
+        for (int i = 0; i < 6; i++) {
+            System.out.println(hello());
+        }
     }
 
 
