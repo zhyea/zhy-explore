0e01394c21d931b3a95209871295d5f319c3f3c3
rui.zhang <rui.zhang@tendcloud.com>
2019-09-24 12:06:48 +0800

    尝试捕捉Throwable，目前未成功，耗时太长，暂搁置
    Change-Id: I195bc8c12c6989c89cb705fe6fe52da4f2f41291

--- asm-explore/src/test/java/org/chobit/asm/MyTest.java
+++ asm-explore/src/test/java/org/chobit/asm/MyTest.java
@@ -5,15 +5,13 @@ public class MyTest {
     public static void main(String[] args) {
         boolean n = false;
         for (int i = 0; i < 6; i++) {
-            System.out.println(hello(n));
+            System.out.println(hello());
         }
         n = true;
         System.out.println(n);
     }
 
-
-    private static String hello(boolean type) {
+    private static String hello() {
         return "Hello";
     }
-
 }
