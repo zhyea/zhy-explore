dd0b089b12607f3d4e01cd0f540f1ced1ebd412b
rui.zhang <rui.zhang@tendcloud.com>
2019-11-08 19:57:16 +0800

    springboot 定时任务
    Change-Id: I446daf9ad63a0ceebee0129a7458569dbb71c15f

---
+++ spring-boot-schedule/src/main/java/org/chobit/spring/task/ScheduledTask.java
@@ -0,0 +1,33 @@
+package org.chobit.spring.task;
+
+
+import org.springframework.boot.autoconfigure.quartz.QuartzDataSource;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+@QuartzDataSource
+@Component
+public class ScheduledTask {
+
+    @Async
+    @Scheduled(fixedRate = 1000)
+    public void job1() {
+        System.out.println(Thread.currentThread().getId() + " ----- job1 ----- " + System.currentTimeMillis());
+    }
+
+    @Async
+    @Scheduled(fixedRate = 1000)
+    public void job2() {
+        System.out.println(Thread.currentThread().getId() + " ----- job2 ----- " + System.currentTimeMillis());
+    }
+
+    @Async
+    @Scheduled(fixedRate = 1000)
+    public void job3() {
+        System.out.println(Thread.currentThread().getId() + " ----- job3 ----- " + System.currentTimeMillis());
+    }
+
+
+}
