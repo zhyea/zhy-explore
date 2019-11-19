ff49360a6eb0a4119186d32ab692653d6c3d8803
rui.zhang <rui.zhang@tendcloud.com>
2019-11-19 09:26:38 +0800

    SpringBoot SpEL表达式
    Change-Id: Id3eb7053f8b86d1748babdadfba5d9b8295cfbbc

--- spring-boot-spel/src/test/java/org/chobit/spring/service/ArithmeticOperatorTest.java
+++ spring-boot-spel/src/test/java/org/chobit/spring/service/ArithmeticOperatorTest.java
@@ -1,8 +1,63 @@
 package org.chobit.spring.service;
 
 import org.chobit.spring.TestBase;
+import org.chobit.spring.operator.ArithmeticOperator;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ArithmeticOperatorTest extends TestBase {
 
+    @Autowired
+    private ArithmeticOperator arithmeticOperator;
 
+    @Test
+    public void add() {
+        Assert.assertEquals(20, arithmeticOperator.getAdd(), 0.00001);
+    }
+
+    @Test
+    public void addString() {
+        Assert.assertEquals("String1 string2", arithmeticOperator.getAddString());
+    }
+
+    @Test
+    public void subtract() {
+        Assert.assertEquals(19, arithmeticOperator.getSubtract(), 0.00001);
+    }
+
+    @Test
+    public void multiply() {
+        Assert.assertEquals(20, arithmeticOperator.getMultiply(), 0.00001);
+    }
+
+    @Test
+    public void divide() {
+        Assert.assertEquals(18, arithmeticOperator.getDivide(), 0.00001);
+    }
+
+    @Test
+    public void divideAlphabetic() {
+        Assert.assertEquals(18, arithmeticOperator.getDivideAlphabetic(), 0.00001);
+    }
+
+    @Test
+    public void modulo() {
+        Assert.assertEquals(7, arithmeticOperator.getModulo(), 0.00001);
+    }
+
+    @Test
+    public void moduloAlphabetic() {
+        Assert.assertEquals(7, arithmeticOperator.getModuloAlphabetic(), 0.00001);
+    }
+
+    @Test
+    public void powerOf() {
+        Assert.assertEquals(512, arithmeticOperator.getPowerOf(), 0.00001);
+    }
+
+    @Test
+    public void brackets() {
+        Assert.assertEquals(17, arithmeticOperator.getBrackets(), 0.00001);
+    }
 }
