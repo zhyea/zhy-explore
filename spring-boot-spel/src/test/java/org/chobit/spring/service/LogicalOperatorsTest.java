55ba4dc1e15c739e890522779a0a3213c4b0d35f
rui.zhang@tendcloud.com <rui.zhang@tendcloud.com>
2019-11-22 19:31:45 +0800

    spring spel 表达式学习

---
+++ spring-boot-spel/src/test/java/org/chobit/spring/service/LogicalOperatorsTest.java
@@ -0,0 +1,45 @@
+package org.chobit.spring.service;
+
+import org.chobit.spring.TestBase;
+import org.chobit.spring.operator.LogicalOperators;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class LogicalOperatorsTest extends TestBase {
+
+
+    @Autowired
+    private LogicalOperators operators;
+
+    @Test
+    public void and() {
+        Assert.assertTrue(operators.isAnd());
+    }
+
+    @Test
+    public void andAlphabetic() {
+        Assert.assertTrue(operators.isAndAlphabetic());
+    }
+
+    @Test
+    public void or() {
+        Assert.assertTrue(operators.isOr());
+    }
+
+    @Test
+    public void orAlphabetic() {
+        Assert.assertTrue(operators.isOrAlphabetic());
+    }
+
+    @Test
+    public void not() {
+        Assert.assertFalse(operators.isNot());
+    }
+
+    @Test
+    public void notAlphabetic() {
+        Assert.assertFalse(operators.isNotAlphabetic());
+    }
+
+}
