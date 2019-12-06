d703ad3bc8f943f64fa3da51aa98d4f738bf8bcf
rui.zhang@tendcloud.com <rui.zhang@tendcloud.com>
2019-12-06 09:30:50 +0800

    SpringBoot API请求切入分析

---
+++ spring-boot-pointcut/readme.md
@@ -0,0 +1,5 @@
+# SpringBoot API请求切入
+
+相关文章  
+
+* [SpringBoot api请求切入]()
---
+++ spring-boot-pointcut/src/main/java/org/chobit/spring/MyApplication.java
@@ -0,0 +1,21 @@
+package org.chobit.spring;
+
+import org.chobit.spring.config.ResponseBodyWrapFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class MyApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+
+    @Bean
+    public ResponseBodyWrapFactoryBean getResponseBodyWrap() {
+        return new ResponseBodyWrapFactoryBean();
+    }
+}
