d40a498725cc0ef558e24eb4c8b72d7cb9aa6d02
rui.zhang <zhangrui137@ke.com>
2019-12-24 18:08:03 +0800

    经验证，ByteBuddy满足需求

--- bytebuddy-explore/src/main/java/org/chobit/bytebuddy/TimingInterceptor.java
+++ bytebuddy-explore/src/main/java/org/chobit/bytebuddy/TimingInterceptor.java
@@ -1,11 +1,11 @@
 package org.chobit.bytebuddy;
 
-import java.lang.reflect.Method;
-import java.util.concurrent.Callable;
-
 import net.bytebuddy.implementation.bind.annotation.Origin;
 import net.bytebuddy.implementation.bind.annotation.RuntimeType;
 import net.bytebuddy.implementation.bind.annotation.SuperCall;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
 
 public class TimingInterceptor {
 
@@ -13,11 +13,20 @@ public class TimingInterceptor {
     public static Object intercept(@Origin Method method,
                                    @SuperCall Callable<?> callable) throws Exception {
 
+        Timing timing = method.getAnnotation(Timing.class);
+        String name = timing.value();
+        if ("".equals(name)) {
+            name = method.toString();
+        }
+
         long start = System.currentTimeMillis();
         try {
             return callable.call();
+        } catch (Throwable t) {
+            TimingClerk.updateFailed(name, System.currentTimeMillis() - start);
+            throw t;
         } finally {
-            TimingClerk.update(method.toString(), System.currentTimeMillis() - start);
+            TimingClerk.update(name, System.currentTimeMillis() - start);
         }
     }
 
