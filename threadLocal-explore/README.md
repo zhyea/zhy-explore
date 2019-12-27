c78a73ce0e7e8835fb59a322b4ad662716d34a58
rui.zhang <zhangrui137@ke.com>
2019-12-27 19:33:56 +0800

    ThreadLocal使用及分析

---
+++ threadLocal-explore/README.md
@@ -0,0 +1,4 @@
+# ThreadLocal-explore
+
+
+用于测试java `ThreadLocal` 类的使用
---
+++ threadLocal-explore/src/main/java/org/chobit/core/tools/Strings.java
@@ -0,0 +1,48 @@
+package org.chobit.core.tools;
+
+import java.util.UUID;
+
+/**
+ * 字符串操作工具类
+ */
+public abstract class Strings {
+
+
+    /**
+     * 生成UUID字符串
+     */
+    public static String uuid() {
+        return UUID.randomUUID().toString();
+    }
+
+
+    /**
+     * 判断字符串是否不为空
+     */
+    public static boolean isNotBlank(final CharSequence cs) {
+        return !isBlank(cs);
+    }
+
+
+    /**
+     * 判断字符串是否为空
+     */
+    public static boolean isBlank(final CharSequence cs) {
+        int strLen;
+        if (cs == null || (strLen = cs.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(cs.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private Strings() {
+        throw new UnsupportedOperationException("Private constructor, cannot be accessed.");
+    }
+
+}
