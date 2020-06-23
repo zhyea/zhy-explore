0dc11568fb7704c7bef2fb743c5892650ea8b44c
rui.zhang <zhangrui137@ke.com>
2020-06-23 17:04:28 +0800

    添加spring-boot-redis使用案例

---
+++ spring-boot-redis/src/main/java/org/chobit/spring/runner/RedisRunner.java
@@ -0,0 +1,59 @@
+package org.chobit.spring.runner;
+
+import org.chobit.spring.cache.RedisClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+
+
+@Component
+public class RedisRunner implements CommandLineRunner {
+
+    @Autowired
+    private RedisClient client;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Please enter cmd:");
+        try (Scanner scanner = new Scanner(System.in)) {
+            OUT:
+            while (true) {
+                String line = scanner.nextLine();
+                if (isBlank(line)) {
+                    continue;
+                }
+                String[] arr = line.split(" ");
+                switch (arr[0]) {
+                    case "get":
+                        System.out.println(client.get(arr[1]));
+                        break;
+                    case "hget":
+                        System.out.println(client.hget(arr[1], arr[2]));
+                        break;
+                    case "exit":
+                        break OUT;
+                    default:
+                        System.out.println("Invalid cmd.");
+                }
+            }
+
+            System.out.println("Bye!");
+        }
+    }
+
+    private boolean isBlank(String str) {
+        int strLen;
+        if (str == null || (strLen = str.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if ((Character.isWhitespace(str.charAt(i)) == false)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
