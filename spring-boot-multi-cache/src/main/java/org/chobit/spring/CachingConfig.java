2aca5087d98499592ab68079d70894aabe3f8615
zhyea <zhangrui@zhyea.com>
2021-01-30 17:46:56 +0800

    add multi cache
    Change-Id: Idc33842c2dbd8ff13a0b38ef4669b6620e03ff94

---
+++ spring-boot-multi-cache/src/main/java/org/chobit/spring/CachingConfig.java
@@ -0,0 +1,70 @@
+package org.chobit.spring;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.apache.logging.log4j.util.Strings.isNotBlank;
+
+@EnableCaching
+@Configuration
+public class CachingConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(CachingConfig.class);
+
+    @Value("${caching.spec}")
+    private String commonSpec;
+
+    @Bean
+    @ConfigurationProperties("caching.special")
+    public Map<String, String> getSpecialCase() {
+        return new HashMap<>(4);
+    }
+
+    @Bean
+    @Primary
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        if (isNotBlank(this.commonSpec)) {
+            cacheManager.setCacheSpecification(this.commonSpec);
+        }
+        return cacheManager;
+    }
+
+
+    @Bean("special")
+    public CacheManager specialCacheManager(Map<String, String> cases) {
+        SimpleCacheManager manager = new SimpleCacheManager();
+        if (cases != null) {
+            List<CaffeineCache> caches =
+                    cases.entrySet().stream()
+                            .map(e -> buildCaffeineCache(e.getKey(), e.getValue()))
+                            .collect(Collectors.toList());
+            manager.setCaches(caches);
+        }
+        return manager;
+    }
+
+    private CaffeineCache buildCaffeineCache(String name, String spec) {
+        logger.info("Cache {} specified with config:{}", name, spec);
+        final Caffeine<Object, Object> caffeineBuilder = Caffeine.from(spec);
+        return new CaffeineCache(name, caffeineBuilder.build());
+    }
+
+
+}
---
+++ spring-boot-multi-cache/src/main/java/org/chobit/spring/service/MasterService.java
@@ -0,0 +1,34 @@
+package org.chobit.spring.service;
+
+
+import org.chobit.spring.bean.Worker;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@CacheConfig(cacheNames = "master")
+public class MasterService {
+
+
+    private List<Worker> workers;
+
+
+    public MasterService() {
+        this.workers = new ArrayList<>(8);
+        workers.add(new Worker("robin", 33));
+        workers.add(new Worker("raccoon", 23));
+        workers.add(new Worker("tom", 16));
+        workers.add(new Worker("HanMeimei", 16));
+    }
+
+    @Cacheable(key = "'get'+#id")
+    public Worker get(int id) {
+        System.out.println("------------------------" + id);
+        return workers.get(id - 1);
+    }
+
+}
