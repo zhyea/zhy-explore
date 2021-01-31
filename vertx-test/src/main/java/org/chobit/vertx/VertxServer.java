4e0dbc3c942541ba926a76737d1460f0b5fb1242
zhyea <zhangrui@zhyea.com>
2021-01-31 22:47:42 +0800

    vertx explore
    Change-Id: I2059acd7fd4e28c5679a6c4f9d02a58d7612a9e2

--- vertx-test/src/main/java/org/chobit/vertx/VertxServer.java
+++ vertx-test/src/main/java/org/chobit/vertx/VertxServer.java
@@ -1,5 +1,7 @@
 package org.chobit.vertx;
 
+import io.vertx.core.Context;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpServer;
@@ -12,11 +14,30 @@ public class VertxServer {
 
     public static void main(String[] args) {
         VertxOptions options = new VertxOptions().setWorkerPoolSize(40);
+        DeploymentOptions deployOpts = new DeploymentOptions().setInstances(36);
+
         Vertx vertx = Vertx.vertx(options);
-        vertx.deployVerticle(MyFirstVerticle.class.getName());
 
 
-        vertx.setPeriodic(1000, id -> {
+        Context context = vertx.getOrCreateContext();
+        if (context.isEventLoopContext()) {
+            System.out.println("Context attached to Event Loop");
+        } else if (context.isWorkerContext()) {
+            System.out.println("Context attached to Worker Thread");
+        } else if (!Context.isOnVertxThread()) {
+            System.out.println("Context not attached to a thread managed by vert.x");
+        }
+
+        vertx.deployVerticle(MyFirstVerticle.class.getName(), deployOpts, res -> {
+            if (res.succeeded()) {
+                System.out.println("Deployment id is: " + res.result());
+            } else {
+                System.out.println("Deployment failed!");
+            }
+        });
+
+
+        vertx.setPeriodic(10000, id -> {
             System.out.println("timer fired!");
         });
 
