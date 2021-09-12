7a52007739484d283215c7a9b7ea6e708165bc4a
zhyea <zhangrui@zhyea.com>
2021-09-12 21:13:54 +0800

    add json explore
    Change-Id: Idbce1881687216847aad5f20a8d771f736c4a7f2

--- json-explore/src/test/java/org/chobit/core/ReusableMethodsTest.java
+++ json-explore/src/test/java/org/chobit/core/ReusableMethodsTest.java
@@ -1,7 +1,10 @@
 package org.chobit.core;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.Test;
 
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
 
@@ -12,9 +15,28 @@ public class ReusableMethodsTest {
 
     @Test
     public void rawToJson() throws URISyntaxException {
-        URL url = ReusableMethods.class.getClassLoader().getResource("/a.json");
-        System.out.println(url.toURI());
-        System.out.println(url.getPath());
+        URL url = ReusableMethods.class.getClassLoader().getResource("a.json");
+        if (null == url) {
+            System.out.println("the url is empty");
+            return;
+        }
+        String path = url.getPath();
+        String json = ReusableMethods.rawToJson(path);
+        System.out.println(json);
     }
+
+
+    @Test
+    public void read(){
+        InputStream in = ReusableMethods.class.getClassLoader().getResourceAsStream("a.json");
+        DocumentContext ctx = JsonPath.parse(in);
+        JsonPath jp = JsonPath.compile("$.store.book");
+        Object obj = ctx.read(jp);
+        System.out.println(obj);
+    }
+
+
+
+
 
 }
