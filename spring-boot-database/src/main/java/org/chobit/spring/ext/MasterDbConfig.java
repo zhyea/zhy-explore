bc30dcae3bf02fc7b2aefa7247b25a1b0a8df32b
zhyea <zhangrui@zhyea.com>
2022-05-13 16:20:45 +0800

    多数据源配置 - 添加自定义配置

--- spring-boot-database/src/main/java/org/chobit/spring/ext/MasterDbConfig.java
+++ spring-boot-database/src/main/java/org/chobit/spring/ext/MasterDbConfig.java
@@ -2,7 +2,6 @@ package org.chobit.spring.ext;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -16,7 +15,7 @@ import javax.sql.DataSource;
 @Configuration
 @MapperScan(basePackages = "org.chobit.spring.service.mapper.master",
         sqlSessionFactoryRef = "masterSqlSessionFactory")
-public class MasterDbConfig {
+public class MasterDbConfig extends AbstractDbConfig {
 
     @Bean(name = "masterDataSource")
     @ConfigurationProperties(prefix = "custom.datasource.master")
@@ -34,9 +33,6 @@ public class MasterDbConfig {
 
     @Bean(name = "masterSqlSessionFactory")
     public SqlSessionFactory setSqlSessionFactory(@Qualifier("masterDataSource") DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-        bean.setDataSource(dataSource);
-
-        return bean.getObject();
+        return buildSqlSessionFactory(dataSource);
     }
 }
