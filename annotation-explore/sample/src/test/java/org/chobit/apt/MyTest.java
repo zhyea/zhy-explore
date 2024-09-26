bf9a0afffd94bcd4b5dd9805627789cd34a544a6
zhyea <zhangrui@zhyea.com>
2024-09-26 15:35:56 +0800

    1. 调整演示的case

--- annotation-explore/sample/src/test/java/org/chobit/apt/MyTest.java
+++ annotation-explore/sample/src/test/java/org/chobit/apt/MyTest.java
@@ -1,7 +1,11 @@
 package org.chobit.apt;
 
 import org.chobit.core.Person;
+import org.chobit.core.School;
 import org.chobit.core.Student;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MyTest {
 
@@ -12,14 +16,52 @@ public class MyTest {
 
         System.out.println(p);
 
+        School school = buildSchoolInstance();
 
-        Student s = new Student();
-        s.setName("tom");
-        s.setAge(13);
-        s.setNo("20070771");
+        System.out.println(school);
 
-        System.out.println(s);
+    }
 
+
+    private static School buildSchoolInstance() {
+        School school = new School();
+        school.setName("文化创意小学");
+        school.setLocation("朝阳区北苑路文化创意大厦");
+
+        List<Student> students = new LinkedList<>();
+
+        Student s1 = new Student();
+        s1.setName("tom");
+        s1.setAge(13);
+        s1.setNo("20070771");
+
+
+        Student s2 = new Student();
+        s2.setName("Jerry");
+        s2.setAge(8);
+        s2.setNo("20220221");
+
+
+        Student s3 = new Student();
+        s3.setName("Harry");
+        s3.setAge(16);
+        s3.setNo("20240012");
+
+
+        Student s4 = new Student();
+        s4.setName("Polly");
+        s4.setAge(3);
+        s4.setNo("20221214");
+
+
+        students.add(s1);
+        students.add(s2);
+        students.add(s3);
+        students.add(s4);
+
+        school.setStudents(students);
+
+        return school;
     }
 
 }
